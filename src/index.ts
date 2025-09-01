c5238b4a1311ddacdbaa2c6ce364ba5dd29d332a
RafaelDeba <rafael.carvalho3@estudante.ifms.edu.br>
2025-09-01 09:46:39 -0300

    aula 01/09

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,60 @@
-import 'dotenv/config'
-console.log(process.env.DBUSER);
+import 'dotenv/config';
+import mysql from 'mysql2/promise';
 
-import express from 'express';
+console.log(process.env.DBHOST);
+import  express, {Request, Response} from 'express';
+
+ 
 const app = express();
+{
+    app.get('/', async (req:Request, res:Response) => {
+        if(!process.env.DBUSER){
+            res.status(500).send('DBUSER não está definido');
+            return;
+        }
+        if(process.env.DBPASSWORD==undefined){
+            res.status(500).send('DBPASSWORD não está definido');
+            return;
+        }
+        if(!process.env.DBHOST){
+            res.status(500).send('DBHOST não está definido');
+             return;
+        }
+        if(!process.env.DBDATABASE){
+            res.status(500).send('DBDATABASE não está definido');
+             return ;
+        }
+        if(!process.env.DBPORT){
+            res.status(500).send('DBPORT não está definido');
+            return;
 
-app.get('/', (req, res) => {
-  res.send('process.env.DBUSER');
-});
+        }
+        try{
+                const connection =  await mysql.createConnection({
+                host: process.env.DBHOST,
+                user: process.env.DBUSER,
+                password: process.env.DBPASSWORD,
+                database: process.env.DBDATABASE,
+                port: Number(process.env.DBPORT)
+                
+            });   
+            res.send("Conectado ao banco de dados com sucesso!");
+            await connection.end();
+        }
+   catch(error){
+    console.error('Erro ao conectar ao banco de dados:', error);
+    res.status(500).send('Erro ao conectar ao banco de dados');
+    return;
+    }
+
+     
+    console.log('DBHOST:', process.env.DBHOST);
+    console.log('DBUSER:', process.env.DBUSER);
+    res.send('Olá ' + process.env.DBUSER);
+    
+    });
+}
 app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+    console.log(`A porta que está rodando é ${8000}`);
 });
+export default app;
