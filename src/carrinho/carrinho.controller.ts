626892c8358f8779aeeb10b0876d467e6c9a3b64
RafaelDeba <rafael.carvalho3@estudante.ifms.edu.br>
2025-09-23 12:21:08 -0300

    TarefaTere

--- src/carrinho/carrinho.controller.ts
+++ src/carrinho/carrinho.controller.ts
@@ -1,17 +1,84 @@
 import { Request, Response } from "express";
 import { db } from "../database/banco-mongo.js";
+import { ObjectId } from "mongodb";
+
+interface ItemCarrinho {
+  produtoId: string;
+  quantidade: number;
+  precoUnitario: number;
+  nome: string;
+}
+
+interface Carrinho {
+  usuarioId: string;
+  itens: ItemCarrinho[];
+  dataAtualizacao: Date;
+  total: number;
+}
 
 class CarrinhoController {
   async adicionar(req: Request, res: Response) {
-    const carrinho = req.body;
-    const resultado = await db.collection('carrinhos').insertOne(carrinho);
-    res.status(201).json({ ...carrinho, _id: resultado.insertedId });
+    const { usuarioId, produtoId, quantidade, precoUnitario, nome } = req.body;
+
+     let carrinho = await db.collection('carrinhos').findOne({ usuarioId });
+
+    const item: ItemCarrinho = {
+      produtoId,
+      quantidade,
+      precoUnitario,
+      nome
+    };  
+
+    if (!carrinho) {
+      // Se o carrinho não existir, cria um novo
+      const carrinho = {
+        usuarioId,
+        itens: [item],
+        dataAtualizacao: new Date(),
+        total: item.precoUnitario * item.quantidade
+      };
+      await db.collection('carrinhos').insertOne(carrinho);
+    } else {
+      // Se o carrinho existir, adiciona o item
+      carrinho.itens.push(item);
+      carrinho.total += item.precoUnitario * item.quantidade;
+      carrinho.dataAtualizacao = new Date();
+      await db.collection('carrinhos').updateOne({ usuarioId }, { $set: carrinho });
+    }
+
+    res.status(200).json(carrinho);
   }
 
   async listar(req: Request, res: Response) {
     const carrinhos = await db.collection('carrinhos').find().toArray();
     res.status(200).json(carrinhos);
   }
+
+  async removerItem(req: Request, res: Response) {
+    const { id } = req.params;
+    await db.collection('carrinhos').updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { itens: [] } }
+    );
+    res.status(200).json({ message: 'Itens removidos com sucesso' });
+  }
+
+  async atualizarQuantidade(req: Request, res: Response) {
+    const { id } = req.params;
+    const { quantidade } = req.body;
+    await db.collection('carrinhos').updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { quantidade } }
+    );
+    res.status(200).json({ message: 'Quantidade atualizada com sucesso' });
+  }
+
+  async removerCarrinho(req: Request, res: Response) {
+    const { id } = req.params;
+    await db.collection('carrinhos').deleteOne({ _id: new ObjectId(id) });
+    res.status(204).send();
+  }
 }
+
 
 export default new CarrinhoController();
